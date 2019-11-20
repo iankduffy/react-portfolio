878d9e7a375a060bdd78409858761589c749984c
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-11-20 19:59:48 +0000

    built desktop

--- components/about-me.js
+++ components/about-me.js
@@ -1,10 +1,10 @@
 const AboutMe = () => {
   return (
-    <div className="container container__row container__row--v-center u-mar-v-lg">
-    <div className="col-4 u-t-cen u-bor-r u-pad-v-xl">
+    <div className="container container__row container__row--v-center u-mar-v-lg u-pad-h-md">
+    <div className="col-4 u-t-cen u-bor-gen u-pad-v-xl u-order-2@md ">
       <h2>Links</h2>
     </div>
-    <div className="col-8 u-pad-l-lg">
+    <div className="col-8 col-12@md u-pad-l-lg u-pad-l-0@md">
       <h2>About Me</h2>
       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
