568c5f0589257471451c8c8c637e47baad2122a4
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-11-25 19:54:45 +0000

    data

--- components/jobs.js
+++ components/jobs.js
@@ -1,10 +1,11 @@
-const Job = () => {
+
+const Job = (props) => {
   return (
     <div className="container container__row container__row--v-center u-mar-v-lg flex-column-mobile">
-      <div className="col-4@lg col-12@md u-t-cen u-bor-r u-pad-v-0@md u-pad-v-xl@md-min u-pad-b-md u-pad-h-lg@md-min u-t-r u-t-l@md u-pad-h-sm@md">
-        <h2>Company</h2>
-        <p className="h3">Position</p>
-        <p className="h4">Dates</p>
+      <div className="col-4 col-12@md u-t-cen u-bor-r u-pad-v-0@md u-pad-v-xl@md-min u-pad-b-md u-pad-h-lg@md-min u-t-r u-t-l@md u-pad-h-sm@md">
+        <h2>{props.work.companyName}</h2>
+        <p className="h3">{props.work.position}</p>
+        <p className="h4">{props.work.dates}</p>
       </div>
       <div className="col-8 col-12@md u-pad-l-lg u-pad-l-sm@md u-pad-r-sm@md container__row--v-center">
         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
--- components/work-experience.js
+++ components/work-experience.js
@@ -1,12 +1,11 @@
 import Job from '../components/jobs'
+import data from '../data/work-experience.json'
 
 const WorkExperience = () => {
   return (
     <div id="work">
       <h2 className="u-t-cen u-t-l@md u-pad-h-sm@md">Work Experience</h2>
-      <Job/>
-      <Job/>
-      <Job/>
+			{data.map((work, key) => <Job work={work} key={key}/>)}
     </div>
   )
 }
