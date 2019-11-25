a6b6259f7967f06e85877a10c9e1fe5f6132ec68
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-11-25 20:15:12 +0000

    data

--- components/jobs.js
+++ components/jobs.js
@@ -1,14 +1,18 @@
 
 const Job = (props) => {
+  const text = props.work.text
+
   return (
     <div className="container container__row container__row--v-center u-mar-v-lg flex-column-mobile">
       <div className="col-4 col-12@md u-t-cen u-bor-r u-pad-v-0@md u-pad-v-xl@md-min u-pad-b-md u-pad-h-lg@md-min u-t-r u-t-l@md u-pad-h-sm@md">
-        <h2>{props.work.companyName}</h2>
-        <p className="h3">{props.work.position}</p>
-        <p className="h4">{props.work.dates}</p>
+        <a href={props.work.link}>
+          <h2>{props.work.companyName}</h2>
+          <p className="h3">{props.work.position}</p>
+          <p className="h4">{props.work.dates}</p>
+        </a>
       </div>
       <div className="col-8 col-12@md u-pad-l-lg u-pad-l-sm@md u-pad-r-sm@md container__row--v-center">
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        {text.map((p, i) => <p key={i}>{p}</p>)}
       </div>
     </div>
   )
