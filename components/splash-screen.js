19fc7b8c66bc70b4cb227815ad077faefb0c23a7
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-11-26 22:39:25 +0000

    dd cv

--- components/splash-screen.js
+++ components/splash-screen.js
@@ -8,7 +8,7 @@ const SplashScreen = () => (
 					<h1>IAN DUFFY</h1>
           <p className="h2">Web Developer | Leeds</p>
 					<div className="container container__row u-mar-v-md">
-						<a className="c-btn col-5 u-mar-r-md" href="https://www.iankduffy.com/">My CV</a>
+						<a className="c-btn col-5 u-mar-r-md" href="/cv.pdf" target="_blank">My CV</a>
 						<a className="c-btn col-5 u-mar-r-md" href="https://github.com/iankduffy">My Github</a>
 					</div>
 				</div>
