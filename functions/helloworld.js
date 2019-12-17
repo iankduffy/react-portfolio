82a21b0d3ef5cb443d2c416cfeb344249b034695
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-17 17:55:40 +0000

    axio

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,5 +1,25 @@
 import axios from 'axios'
 
 exports.handler = function(event, context, callback) {
-  axios.get(`https://graph.instagram.com/me/media?&fields=id,caption,media_type,media_url,permalink&access_token=${process.env.INSTAGRAM}`)
+  axios.get(`https://graph.instagram.com/me/media?&fields=id,caption,media_type,media_url,permalink&access_token=${process.env.INSTAGRAM}&count=4`)
+  .then(({ data: { data: posts } }) => {
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(
+        posts.map(i => ({
+          id: i.id,
+          link: i.link,
+          images: i.images,
+          videos: i.videos,
+          caption: i.caption.text,
+        })),
+      ),
+    })
+  })
+  .catch((e) => {
+    return e
+  })
 };
