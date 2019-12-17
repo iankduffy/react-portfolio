f094991f49d0d8d7b1951587b46948bafff445ac
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-17 17:50:58 +0000

    data

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
 exports.handler = function(event, context, callback) {
-    const data = axios.get(`https://graph.instagram.com/me/media?&fields=id,caption,media_type,media_url,permalink&access_token=${process.env.INSTAGRAM}`)
-    .then(data => {return data})
-    console.log("hi Ian")
+  axios.get(`https://graph.instagram.com/me/media?&fields=id,caption,media_type,media_url,permalink&access_token=${process.env.INSTAGRAM}`)
 };
