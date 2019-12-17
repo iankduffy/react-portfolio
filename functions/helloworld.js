0279942b0721fda7facf45d080a2206e9b0a2fb4
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-17 18:29:11 +0000

    fdf

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 exports.handler = function(event, context, callback) {
-  axios.get(`https://graph.instagram.com/me/media?&fields=id,caption,media_type,media_url,permalink&access_token=${process.env.INSTAGRAM}&count=4`)
+  axios.get(`https://graph.instagram.com/me/media?&fields=id,caption,media_type,media_url,permalink&access_token=${process.env.INSTAGRAM}`)
   .then(({data}) => {
     callback(null, {
       statusCode: 200,
