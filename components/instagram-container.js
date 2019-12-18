666f334864c51210da59b9a0a9ec78020e2f3573
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-18 18:36:23 +0000

    data

--- components/instagram-container.js
+++ components/instagram-container.js
@@ -2,21 +2,24 @@ import InstagramImage from '../components/instagram-photo'
 import {useState} from 'react'
 import axios from 'axios'
 
-const InstagramContainer = (props) => {
- 
-  console.log(data)
+const InstagramContainer = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => fetchData(), []);
+
+  const fetchData = async () => {
+    const data = await axios.get(`/.netlify/functions/helloworld`)
+    console.log(data)
+    setData(data)
+  }
+  
+  fetchData()
+
   return (
     <div className="container--fluid u-mar-t-md container__row">
       {Array(4).fill(<InstagramImage/>)}
     </div>
   )
-}
-
-InstagramContainer.getInitialProps = async ({ req }) => {
-  const res = await axios.get(`/.netlify/functions/helloworld`)
-  const json = await res.json()
-  
-  return { data: json }
 }
   
 export default InstagramContainer;
