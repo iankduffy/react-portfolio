356761f2c195891a5d1037abd7c95e6478ce88c9
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-18 18:32:51 +0000

    data

--- components/instagram-container.js
+++ components/instagram-container.js
@@ -2,8 +2,8 @@ import InstagramImage from '../components/instagram-photo'
 import {useState} from 'react'
 import axios from 'axios'
 
-const InstagramContainer = () => {
-
+const InstagramContainer = (props) => {
+ 
   console.log(data)
   return (
     <div className="container--fluid u-mar-t-md container__row">
