b9ba05f5275158106c450a623728fc4f8d5ffe7a
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-18 17:56:15 +0000

    df

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,12 +1,16 @@
 import axios from 'axios'
 
 exports.handler = function(event, context, callback) {
-
-  axios.get(`https://graph.instagram.com/me/media?&fields=id,caption,media_type,media_url,permalink&access_token=${process.env.INSTAGRAM}`)
-  .then(({data}) => {
-    return data
+  // https://graph.instagram.com/me/media?&fields=id,caption,media_type,media_url,permalink&access_token=IGQVJXdG1paFpfb3JoSHNId2ZAUSnNlakNjcmhHWEc5WEx2ZAnM2Vm9EMGFjdndfT0NkXzltZAGhtWE5VRXplbV9UcjZAzSDNiV0djTEhoTGFxUXRCSk9nWEJHcUFBWk5WYmJfblpoTV96VHhPb09zdFNJV25BMTlzM25iRHdZA
+  // 1906409028.b570245.7f5a7328e69b495d80ea715740b84c8e
+  axios.get(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${process.env.INSTAGRAM}&fields=id,caption,media_type,media_url,permalink&count=4`)
+  .then((res) => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(res.data),
+    });
   })
   .catch((e) => {
-    console.log('alert')
+    console.log(e)
   })
 };
