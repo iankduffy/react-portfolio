067439f8006f6f3c7d4928466277e52484ebbdac
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-18 18:27:26 +0000

    data

--- components/instagram-container.js
+++ components/instagram-container.js
@@ -3,21 +3,20 @@ import {useState} from 'react'
 import axios from 'axios'
 
 const InstagramContainer = () => {
-  const [data, setData] = useState([])
 
-  const fetchData = async () => {
-    const data = await axios.get(`/.netlify/functions/helloworld`)
-    console.log(data)
-    setData(data)
-  }
-  
-  fetchData()
-
+  console.log(data)
   return (
     <div className="container--fluid u-mar-t-md container__row">
       {Array(4).fill(<InstagramImage/>)}
     </div>
   )
+}
+
+InstagramContainer.getInitialProps = async ({ req }) => {
+  const res = await axios.get(`/.netlify/functions/helloworld`)
+  const json = await res.json()
+  
+  return { data: json }
 }
   
 export default InstagramContainer;
--- functions/helloworld.js
+++ functions/helloworld.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 exports.handler = function(event, context, callback) {
   // https://graph.instagram.com/me/media?&fields=id,caption,media_type,media_url,permalink&access_token=IGQVJXdG1paFpfb3JoSHNId2ZAUSnNlakNjcmhHWEc5WEx2ZAnM2Vm9EMGFjdndfT0NkXzltZAGhtWE5VRXplbV9UcjZAzSDNiV0djTEhoTGFxUXRCSk9nWEJHcUFBWk5WYmJfblpoTV96VHhPb09zdFNJV25BMTlzM25iRHdZA
-  // 1906409028.b570245.7f5a7328e69b495d80ea715740b84c8e
   axios.get(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${process.env.INSTAGRAM}&fields=id,caption,media_type,media_url,permalink&count=4`)
   .then((res) => {
     callback(null, {
