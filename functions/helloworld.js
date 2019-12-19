73225739f66b716523853088a9dd791707d3cd7f
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-19 15:15:24 +0000

    cache

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -7,6 +7,9 @@ exports.handler = function(event, context, callback) {
     callback(null, {
       statusCode: 200,
       body: JSON.stringify(res.data.data),
+      headers: {
+        'Cache-Control': 'public, s-maxage=86400',
+      },
     });
   })
   .catch((e) => {
