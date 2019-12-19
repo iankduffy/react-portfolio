e02397fa84350788824eefb2e4b78277c5617c00
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-19 12:10:08 +0000

    string

--- components/instagram-container.js
+++ components/instagram-container.js
@@ -11,7 +11,7 @@ const InstagramContainer = () => {
       const res = await axios.get(`/.netlify/functions/helloworld`)
       // const res = axios.get(`https://api.instagram.com/v1/users/self/media/recent/?access_token=$1906409028.b570245.7f5a7328e69b495d80ea715740b84c8e&fields=id,caption,media_type,media_url,permalink&count=4`)
       console.log({res})
-      setData(JSON.parse(res))
+      setData(JSON.parse(JSON.stringify(res)))
     }
 
     fetchData()
