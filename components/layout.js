8136baa8c769b77c536adf633192764d29ab141e
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-19 20:50:29 +0000

    meta

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,9 @@ export default ({ children, title = 'Ian Duffy | Web Developer' }) => (
     <Head>
       <title>{title}</title>
       <meta charSet='utf-8' />
+      <meta name="description" content="Ian Duffy - Web Developer Based in Leeds"/>
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta name="theme-color" content="#223a57"/>
     </Head>
     <div className="u-bg-grey">
       <Nav/>
