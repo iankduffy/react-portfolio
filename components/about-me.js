b5f4e0004b595b4b98367b35bcddc59ca4e82ad1
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-19 23:09:50 +0000

    update

--- components/about-me.js
+++ components/about-me.js
@@ -22,11 +22,11 @@ const AboutMe = () => {
     </div>
     <div className="col-8 col-12@md u-pad-l-lg u-pad-l-0@md">
       <h2>About Me</h2>
-      <p>I am a Web Developer based in Leeds, West Yorkshire. Who enjoys designing and building websites, with multiple languages and tools. Currently working with Ruby on Rails site, with multiple side projects using vanilla Javascript, reactJS and other frameworks.</p>
-      <p>I moved up to Leeds for University in 2012, where I studied Creative Media Technology at Leeds Metropolitan University, to gradute in 2015 with a 2.1. In the Course I had to develop and demonstrate both design and programming skills in variety of projects such as building responsive dynamic websites, building mobile game in Unity, both 2D and 3D animation, and visual brand communication.</p>
-      <p>Outside of work, I work on multiple projects including both design and development, going the gym, playing rugby and doing gymanstics, aswell as attending events like LeedsJS and Leeds Digital Drinks.</p>
+      <p>I am a Web Developer based in Leeds, West Yorkshire. Who enjoys designing and building websites, using multiple languages and tools. Currently working on Ruby on Rails site with ES6 and SASS, with multiple side projects using Javascript, ReactJS and other frameworks.</p>
+      <p>I moved up to Leeds for University in 2012, where I studied Creative Media Technology at Leeds Metropolitan University, to gradute in 2015 with a 2.1 Degree Classification. In my Course I had to develop and demonstrate both my design and programming skills in variety of projects such as building responsive dynamic websites, building mobile game in Unity, both 2D and 3D animation, and visual brand communication.</p>
+      <p>Outside of work, I work on multiple projects including both design and development helping pushing my skills forward, going the gym, playing rugby and doing gymanstics, aswell as attending events like LeedsJS and Leeds Digital Drinks.</p>
       <div className="container__row container--space-between u-mar-t-md o-flex-al-stretch">
-        {Skills.map((skill, key) => <div key={key} className="skill container__column "><img src={`/${imageName(skill)}-logo.svg`} alt={skill}/><span>{skill}</span></div>)}
+        {Skills.map((skill, key) => <div key={key} className="skill container__column "><img src={`/${imageName(skill)}-logo.svg`} alt={skill} lazy="true"/><span>{skill}</span></div>)}
       </div>
     </div>
   </div>
