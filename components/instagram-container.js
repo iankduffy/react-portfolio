6ecd08c36044d5b18ebbc56e78b240dabf45eef3
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-19 12:37:26 +0000

    then

--- components/instagram-container.js
+++ components/instagram-container.js
@@ -1,18 +1,22 @@
 import InstagramImage from '../components/instagram-photo'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-// import instagramData from '../data/instragram-data'
+import instagramData from '../data/instragram-data'
 
 const InstagramContainer = () => {
   const [data, setData] = useState([])
   // console.log(JSON.parse(JSON.stringify(instagramData)))
   useEffect(() => {
     const fetchData = async () => {
-      const res = await axios.get(`/.netlify/functions/helloworld`)
-      // const res = axios.get(`https://api.instagram.com/v1/users/self/media/recent/?access_token=$1906409028.b570245.7f5a7328e69b495d80ea715740b84c8e&fields=id,caption,media_type,media_url,permalink&count=4`)
-      let newData = JSON.parse(JSON.stringify(res))
-      setData(newData.data)
-      console.log(data)
+      await axios.get(`/.netlify/functions/helloworld`)
+      .then(data => {
+        let newData = JSON.parse(JSON.stringify(data))
+        setData([newData.data])
+        console.log(data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
     }
 
     fetchData()
