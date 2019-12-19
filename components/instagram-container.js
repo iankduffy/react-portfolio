c93dd172df9d8c7db25f266e707955d5eeb19963
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-19 15:00:53 +0000

    fix

--- components/instagram-container.js
+++ components/instagram-container.js
@@ -13,7 +13,7 @@ const InstagramContainer = () => {
         let newData = JSON.parse(JSON.stringify(res))
         let images = newData.data.data
         console.log({newData})
-        setImages([images])
+        setImages(images)
         console.log({images})
       })
       .catch(err => {
