a8597a10fb353ab360c396e95f99e9801c5e6dfb
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-19 20:31:13 +0000

    label for link

--- components/instagram-photo.js
+++ components/instagram-photo.js
@@ -6,7 +6,7 @@ const InstagramImage = ({image}) => (
     <div className="c-instagram--post" style={{backgroundImage: `url(${image.images.standard_resolution.url})`}}>
     </div>
     <div className="c-instagram--text u-pad-h-sm u-pad-v-sm u-pad-h-lg@md-min u-t-cen">
-        {image.caption.text}
+      {image.caption.text}
     </div>
   </div>
 );
--- components/jobs.js
+++ components/jobs.js
@@ -5,7 +5,7 @@ const Job = (props) => {
   return (
     <div className="container container__row container__row--v-center u-mar-v-lg flex-column-mobile u-no-wrap">
       <div className="col-4 col-12@md u-t-cen u-bor-r u-pad-v-0@md u-pad-v-lg@md-min u-pad-b-md u-pad-h-md@md-min u-pad-h-lg@lg-min u-t-r u-t-l@md u-pad-h-sm@md">
-        <a href={props.work.link} >
+        <a href={props.work.link} aria-label={props.work.companyName}>
           <h2>{props.work.companyName}</h2>
           <p className="h3">{props.work.position}</p>
           <p className="h4">{props.work.dates}</p>
