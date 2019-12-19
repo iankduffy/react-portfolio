9aa5e6fdb13ea26386d4bbd4afd7e676728b208c
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-19 12:20:04 +0000

    commit

--- components/instagram-container.js
+++ components/instagram-container.js
@@ -5,12 +5,12 @@ import axios from 'axios'
 
 const InstagramContainer = () => {
   const [data, setData] = useState([])
-
+  // console.log(JSON.parse(JSON.stringify(instagramData)))
   useEffect(() => {
     const fetchData = async () => {
       const res = await axios.get(`/.netlify/functions/helloworld`)
       // const res = axios.get(`https://api.instagram.com/v1/users/self/media/recent/?access_token=$1906409028.b570245.7f5a7328e69b495d80ea715740b84c8e&fields=id,caption,media_type,media_url,permalink&count=4`)
-      console.log(JSON.parse(res))
+      console.log(JSON.parse(JSON.stringify(res)))
       // setData(JSON.parse(JSON.stringify(res)))
     }
 
