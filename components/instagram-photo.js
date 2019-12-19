e94d2588be90c91607f5e98ad0ee1611a7a27a89
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-19 20:18:11 +0000

    v

--- components/instagram-photo.js
+++ components/instagram-photo.js
@@ -5,7 +5,7 @@ const InstagramImage = ({image}) => (
   <div className="c-instagram">
     <div className="c-instagram--post" style={{backgroundImage: `url(${image.images.standard_resolution.url})`}}>
     </div>
-    <div className="c-instagram--text u-pad-h-sm u-pad-h-lg@md-min u-t-cen">
+    <div className="c-instagram--text u-pad-h-sm u-pad-v-sm u-pad-h-lg@md-min u-t-cen">
         {image.caption.text}
     </div>
   </div>
