f8ff637e4c548dfb8468634e15133b67ebd25234
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-22 17:06:38 +0000

    animate in

--- components/splash-screen.js
+++ components/splash-screen.js
@@ -2,7 +2,7 @@ import ProfilePic from '../components/c-profile-pic'
 
 const SplashScreen = () => (
   <div className="c-screen u-pad-h-md container__row--v-center u-dis-flex" id="home">
-    	<div className="container container__row--v-center flex-column-tablet u-t-cen@lg"> 
+    	<div className="container container__row--v-center flex-column-tablet u-t-cen@lg u-transition-in"> 
 				<div className="col-5 col-12@lg u-mar-r-md@md u-pad-v-md"><ProfilePic/></div>
 				<div className="col-7 col-12@md container container__column">
 					<h1>IAN DUFFY</h1>
