67accd4490a014048b82924611342be1f8f2d623
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-12-22 14:25:55 +0000

    skyscrapers added

--- components/projects.js
+++ components/projects.js
@@ -5,7 +5,7 @@ const Projects = () => {
     <div className="u-mar-v-lg" id="projects">
       <h2 className="u-t-cen">Projects</h2>
       <div className="container container__row u-mar-t-md u-pad-h-sm">
-        <Project image="" title="SkyScrapers Trampoline Club" description="Working with the owner of SkyScrapers Trampoline Club, I designed and built their website using Next Js. The Client wanted the website to provide information for the club." isDisabled={false} link="https://www.skyscraperstrampolineclub.co.uk/"/>
+        <Project image="" title="SkyScrapers Trampoline Club" description="Working with the owner of SkyScrapers Trampoline Club, I designed and built their website using NextJs. The Client wanted the website to provide information for the club." isDisabled={false} link="https://www.skyscraperstrampolineclub.co.uk/"/>
         <Project image="" title="" description="" isDisabled={true}/>
         <Project image="" title="" description="" isDisabled={true}/> 
       </div>     
